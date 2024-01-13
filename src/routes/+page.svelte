cab00c5fbd2ca7c560c29d6dd45d1a52069eb7b3
Mattias Holmlund <mattias@holmlund.se>
2024-01-13 23:07:09 +0100

    Use acorn to find variable names

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,29 @@
 <script lang="ts">
 	import CodeMirror from 'svelte-codemirror-editor';
 	import { javascript } from '@codemirror/lang-javascript';
+	import * as acorn from 'acorn';
 
 	var code = 'test';
-	var output = '';
+	var output: { [key: string]: string | number } | string = {};
 
 	function run() {
-		const fn = new Function(code);
-		output = fn();
+		const vars = new Array<string>();
+		const res = acorn.parse(code, { ecmaVersion: 2020 });
+		console.log(res);
+		for (const r of res.body) {
+			if (r.type === 'VariableDeclaration') {
+				if (r.declarations[0].id.type === 'Identifier') {
+					vars.push(r.declarations[0].id.name);
+				}
+			}
+		}
+
+		const fn = new Function(code + ';\nreturn {' + vars.join(',') + '}');
+		try {
+			output = fn();
+		} catch (err) {
+			console.log('err');
+		}
 	}
 </script>
 
@@ -33,4 +49,10 @@
 	}}
 />
 
-{output}
+{#if typeof output === 'object'}
+	{#each Object.keys(output) as key}
+		{key}: {output[key]}
+	{/each}
+{:else}
+	{output}
+{/if}
