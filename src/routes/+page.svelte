4ffbba672b186fc545c4c9a05edf5bab8bd18826
Mattias Holmlund <mattias@holmlund.se>
2024-03-10 21:05:53 +0100

    Show errors and allow return in code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,13 @@
 
 	var code = 'const a=1;\nlet b=a+1;\nb++';
 	var output: { [key: string]: string | number } | string = {};
+	let error = '';
 
 	function run(code: string) {
 		const vars = new Array<string>();
 		try {
-			const res = acorn.parse(code, { ecmaVersion: 2020 });
+			error = '';
+			const res = acorn.parse(code, { ecmaVersion: 2020, allowReturnOutsideFunction: true });
 			for (const r of res.body) {
 				if (r.type === 'VariableDeclaration') {
 					if (r.declarations[0].id.type === 'Identifier') {
@@ -22,11 +24,17 @@
 			try {
 				return fn();
 			} catch (err) {
-				console.log('err');
+				console.log('err', err);
+				if (err instanceof Error) {
+					error = err.message;
+				}
 				return '';
 			}
 		} catch (err) {
-			console.log('acorn');
+			if (err instanceof Error) {
+				error = err.message;
+			}
+			console.log('acorn', err);
 		}
 	}
 
@@ -53,12 +61,16 @@
 	</div>
 
 	<div class="flex-child" style="padding: 5px 10px;">
-		{#if typeof output === 'object'}
-			{#each Object.keys(output) as key}
-				<div>
-					<b>{key}</b> = {output[key]}
-				</div>
-			{/each}
+		{#if error}
+			<div style="background-color: pink; padding: 5px;">
+				{error}
+			</div>
+		{:else if typeof output === 'object'}
+			<!-- prettier-ignore -->
+			<pre style="margin-top:0;">
+{#each Object.keys(output) as key}<b>{key}</b> = {JSON.stringify(output[key], undefined, 2)}
+{/each}&nbsp;
+			</pre>
 		{:else}
 			{output}
 		{/if}
